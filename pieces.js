602747a73d0edd12e87dafbccacd808194c8a198
Emil Roydev <125214785+ERoydev@users.noreply.github.com>
2023-11-22 17:55:47 +0200

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -3,7 +3,7 @@ const piecesTypes = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'kni
 function loadPieces (color) {
     let result = {};
     for(let piece of piecesTypes) {
-        let pc = `<div class="piece" id="${piece}"><img src="assets/SVG with shadow/${color}_${piece}_svg_withShadow.svg" height="75" /></div>`;
+        let pc = `<div class="piece ${color}" id="${piece}"><img src="assets/SVG with shadow/${color}_${piece}_svg_withShadow.svg" height="75" /></div>`;
         result[piece] = pc;
     }
     return result
